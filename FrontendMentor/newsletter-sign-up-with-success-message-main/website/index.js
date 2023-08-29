d8b45a880fa6d5a0fa690e985d3f5283fb33dd6e
radusofron <radu.sofron169@gmail.com>
2023-08-29 22:53:48 +0300

    Frontend mentor 1st project code done

--- FrontendMentor/newsletter-sign-up-with-success-message-main/website/index.js
+++ FrontendMentor/newsletter-sign-up-with-success-message-main/website/index.js
@@ -1,14 +1,54 @@
+// Extract images
+const desktopImage = document.getElementById("desktop")
+const mobileImage = document.getElementById("phone")
+
+function uploadProperImage() {
+    // Extract window width
+    const windowInnerWidth  = window.innerWidth;
+    // Decide what image to upload
+    if (windowInnerWidth <= 1000) {
+        desktopImage.classList.add("hide")
+    }
+    else {
+        mobileImage.classList.add("hide")
+    }
+}
+// Upload proper image at the begining
+uploadProperImage()
+
+
+// Upload proper image when resizing
+// Add event listener for webpage
+window.addEventListener("resize", function() {
+    // Extract current window width
+    let currentWindowInnerWidth  = window.innerWidth;
+    // Decide what image to upload
+    if (currentWindowInnerWidth <= 1000) {
+        if (!desktopImage.classList.contains("hide")) {
+            desktopImage.classList.add("hide")
+            mobileImage.classList.remove("hide")
+        }
+    }
+    else {
+        if (!mobileImage.classList.contains("hide")) {
+            mobileImage.classList.add("hide")
+            desktopImage.classList.remove("hide")
+        }
+    }
+});
+
+
+// Verify email format and act accordingly
 // Extract form
 const form = document.getElementById("form")
-// Extract input
-const inputElement = document.getElementById("email")
-// Extract error message
-const errorMessage = document.getElementById("error")
-
 // Add event listener for form
 form.addEventListener("submit", function(ev) {
+    // Extract input
+    const inputElement = document.getElementById("email")
     // Extract email
     const inputEmail = inputElement.value
+    // Extract error message
+    const errorMessage = document.getElementById("error")
     // Create regex
     const emailPattern = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/)
     // Verify email format
@@ -19,4 +59,4 @@ form.addEventListener("submit", function(ev) {
         // Pass default form action
         ev.preventDefault()
     }
-})
+});
